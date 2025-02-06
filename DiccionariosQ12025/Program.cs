96e725aa6560e0295a973102a37441572dc79726
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-05 19:18:34 -0600

    add dicctionary

--- DiccionariosQ12025/Program.cs
+++ DiccionariosQ12025/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<int, string> dic = new Dictionary<int, string>()
+{
+    { 1, "Juan Perez"},
+    {2,"Carlos Perez" },
+    {3,"Maria Perez" },
+};
+
+dic.Add(4, "Jesus Ortega");
+dic.Add(5, "Presentacion Lopez");
+
+foreach (var item in dic) { 
+Console.WriteLine(item);
+
+} ;
+
